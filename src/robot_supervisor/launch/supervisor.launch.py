fd7b954972550cf3b7db93191096eb689e371d57
tranmanhhieu-hue <dulieucannho1@gmail.com>
2026-05-13 08:59:17 +0700

    Update robot source code

--- src/robot_supervisor/launch/supervisor.launch.py
+++ src/robot_supervisor/launch/supervisor.launch.py
@@ -15,23 +15,23 @@ def generate_launch_description():
             output="screen",
             emulate_tty=True,
         ),
-        # Node(
-        #     package='robot_supervisor',
-        #     executable='navigation_alert_node',
-        #     name='navigation_alert_node',
-        #     output='screen',
-        #     parameters=[{
-        #         'path_topic': '/plan',
-        #         'scan_topic': '/scan',
-        #         'alert_topic': '/nav_alert_text',
-        #         'robot_frame': 'base_link',
-        #         'global_frame': 'map',
-        #         'turn_lookahead_m': 1.0,
-        #         'turn_after_m': 0.7,
-        #         'turn_angle_threshold_deg': 35.0,
-        #         'obstacle_distance_m': 1.0,
-        #         'front_angle_deg': 25.0,
-        #         'cooldown_sec': 5.0,
-        #     }]
-        # ),
+        Node(
+            package='robot_supervisor',
+            executable='navigation_alert_node',
+            name='navigation_alert_node',
+            output='screen',
+            parameters=[{
+                'path_topic': '/plan',
+                'scan_topic': '/scan',
+                'alert_topic': '/nav_alert_text',
+                'robot_frame': 'base_link',
+                'global_frame': 'map',
+                'turn_lookahead_m': 1.0,
+                'turn_after_m': 0.7,
+                'turn_angle_threshold_deg': 35.0,
+                'obstacle_distance_m': 1.0,
+                'front_angle_deg': 25.0,
+                'cooldown_sec': 5.0,
+            }]
+        ),
     ])
